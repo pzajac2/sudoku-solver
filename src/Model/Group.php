da89529e4a9d2feeb8e8039084486718217cd4ea
Piotr Zając <piotr.zajac@iqstudio.pl>
2022-02-05 16:29:09 +0100

    Model - Row, Column and Group of Cells

---
+++ src/Model/Group.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SudokuSolver\Model;
+
+use Webmozart\Assert\Assert;
+
+class Group extends CellCollection
+{
+    const SIZE = 3;
+
+    public function getCell(int $x, int $y): Cell
+    {
+        Assert::range($x, 0, 2, 'X coordinate must be between 0..2');
+        Assert::range($y, 0, 2, 'Y coordinate must be between 0..2');
+
+        $id = $y * self::SIZE + $x;
+        return $this->cells[$id];
+    }
+}
